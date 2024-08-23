63dc7fc7a44798ef76398eb29b006208d55b53cd
Miqueias Alves <32490376+Miqueias-Alves@users.noreply.github.com>
2024-08-23 15:42:31 -0400

    chore: config api

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,27 @@
-import express, { Express, Request, Response } from "express";
+import cors from "cors";
 import dotenv from "dotenv";
+import express, { Express } from 'express';
+import routes from "./routes";
 
-dotenv.config()
+// Carrega as variáveis de ambiente
+dotenv.config();
 
+// Criando uma instância do express
 const app: Express = express();
 
+// Definindo a porta do servidor
 const port = process.env.PORT || 4000;
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Express + TypeScript Server')
-})
+// Middleware para permitir requisições de qualquer origem
+app.use(cors());
 
+// Middleware para permitir requisições com corpo JSON
+app.use(express.json());
+
+// Middleware para as rotas
+app.use("/api", routes);
+
+// Inicializando o servidor
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`)
 })
