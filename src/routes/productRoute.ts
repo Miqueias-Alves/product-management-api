1d14e25017fc2bd9fc0569030e8c3ac6b16a7541
Miqueias Alves <32490376+Miqueias-Alves@users.noreply.github.com>
2024-08-24 17:20:45 -0400

    feat: create endpoint to update product

--- src/routes/productRoute.ts
+++ src/routes/productRoute.ts
@@ -1,11 +1,12 @@
 import { Router } from "express";
 
-import {findAll, findById, create} from "../controller/productController";
+import {findAll, findById, create, update} from "../controller/productController";
 
 const productRouter: Router = Router();
 
 productRouter.get("/", findAll);
 productRouter.post("/", create);
 productRouter.get("/:id", findById);
+productRouter.put("/:id", update);
 
 export default productRouter;
